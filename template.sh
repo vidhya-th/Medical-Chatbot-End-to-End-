160559926f0b5c9c7984312fe2a2fd1b9137b858
vidhya-th <vidhyath96@gmail.com>
2026-01-16 14:28:21 +0100

    project template created

---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating project directories
+mkdir -p src
+mkdir -p research
+
+#creating file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Project structure created successfully."
